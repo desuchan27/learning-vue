0466ee16ff3eedd9071fb3939a59d126382ba33c
Desu <orenodesukouichi@gmail.com>
2024-05-30 00:52:28 +0800

    learned attribute bindings

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -3,10 +3,13 @@
 import NestedComponent from './NestedComponent.vue'
 
 const name = 'Desu JS'
+const imgSrc =
+  'https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/445229025_3720087498279070_8287430768663977059_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFCvPmyhswnH3edXNMzi3nx4s5oH1Qe5gzizmgfVB7mDKfDOYLH9MOjXslkTw9v3YXcc6qK3vLY7A87R8pTUkjM&_nc_ohc=iRMnJ1Qc-FgQ7kNvgHmSAol&_nc_ht=scontent.fceb6-1.fna&oh=00_AYCsNCZdssHkhFScfi9gLbFPqmlhxUQuBmXQve2Il9TWNg&oe=665D19DE'
 </script>
 
 <!-- HTML  -->
 <template>
+  <img :src="imgSrc" alt="Desu's avatar" />
   <div>
     <h1>{{ name }}</h1>
     <NestedComponent />
@@ -18,5 +21,10 @@ const name = 'Desu JS'
 h1 {
   font-weight: 900;
   color: #999;
+}
+img {
+  width: 100%;
+  border-radius: 50%;
+  display: block;
 }
 </style>
--- src/components/NestedComponent.vue
+++ src/components/NestedComponent.vue
@@ -2,10 +2,14 @@
 const age = 21
 const occupation = 'Web Developer'
 const bio = 'I aim to be productive each day (except Sundays 😜).'
+const githubLink = 'https://github.com/desuchan27'
 </script>
 
 <template>
   <p>{{ age }}</p>
   <p>{{ occupation }}</p>
   <p>{{ bio }}</p>
+  <a :href="githubLink" target="_blank">GitHub</a>
 </template>
+
+<style scoped></style>
