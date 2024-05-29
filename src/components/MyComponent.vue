8f3dcd0e2fbd781539b21199874490ed558687ce
Desu <orenodesukouichi@gmail.com>
2024-05-30 00:31:20 +0800

    learned main component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,22 @@
+<!-- JS -->
+<script setup>
+import NestedComponent from './NestedComponent.vue'
+console.log('Hello World')
+</script>
+
+<!-- HTML  -->
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <NestedComponent />
+  </div>
+</template>
+
+<!-- CSS  -->
+<style scoped>
+h1 {
+  font-weight: 900;
+  color: #999;
+  text-align: center;
+}
+</style>
