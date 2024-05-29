9c1d55aa54589549acac6195d9f28ef184f71d1e
Desu <orenodesukouichi@gmail.com>
2024-05-30 00:31:32 +0800

    added nested component

---
+++ src/components/NestedComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Nested Component</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ab dolor officia quia maxime
+    suscipit consectetur, labore facilis neque vitae?
+  </p>
+</template>
