687d64589602ae4c321b0059f92d0e1aad60254b
Desu <orenodesukouichi@gmail.com>
2024-05-30 16:12:08 +0800

    localstyle

--- src/components/LocalStyle.vue
+++ src/components/LocalStyle.vue
@@ -1,3 +1,11 @@
 <template>
   <h2 class="text-red">This is children component</h2>
+  <p class="text-blue">Trying local scope in this component</p>
 </template>
+
+<!-- "scoped" is the local modifier for styling -->
+<style scoped>
+.text-blue {
+  color: blue;
+}
+</style>
